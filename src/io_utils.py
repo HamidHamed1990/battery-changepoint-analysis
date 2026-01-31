51cd6810a7e89f495a5adce26cbc24c606a296f8
HamidHamed1990 <137899024+HamidHamed1990@users.noreply.github.com>
2026-01-31 15:18:05 +0100

    Update io_utils.py

--- src/io_utils.py
+++ src/io_utils.py
@@ -1,1 +1,11 @@
 # Input/output utilities
+import pandas as pd
+
+def load_excel_sheets(file_path):
+    sheets = pd.read_excel(file_path, sheet_name=None)
+    return (
+        sheets["capacity"],
+        sheets["voltage"],
+        sheets["dQdV"],
+        sheets["dVdQ"],
+    )
